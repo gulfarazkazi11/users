3d770f45324a5652b32dbbbab3c5f28325a6bc91
gulfarazkazi11 <gulfarazkazi11@gmail.com>
2026-05-07 01:59:00 +0530

    Add User class with properties

--- user.cs
+++ user.cs
@@ -1,1 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserForm
+{
+    internal class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public int Age { get; set; }
+    }
+}
 
