03a3ab118488efefb7e1378d1d63a21088be9f80
gulfarazkazi11 <gulfarazkazi11@gmail.com>
2026-05-07 01:57:15 +0530

    Implement DbHelper class for SQL connection
    Added a DbHelper class for database connection management.

--- DbHelper.cs
+++ DbHelper.cs
@@ -1,1 +1,19 @@
-first 
+using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserForm
+{
+    internal class DbHelper
+    {
+        private string connectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=F:\\coding\\EntityFrame\\Src\\DbOprationWithEfCore\\UserForm\\userDB.mdf;Integrated Security=True";
+
+        public SqlConnection GetConnection()
+        {
+            return new SqlConnection(connectionString);
+        }
+    }
+}
